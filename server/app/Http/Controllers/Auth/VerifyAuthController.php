19018eedc2692713e676063b71badbcd4db8761c
take-cantik <take.cantik17@gmail.com>
2024-05-10 20:07:28 +0900

    :sparkles: Add VerifyAuthController

---
+++ server/app/Http/Controllers/Auth/VerifyAuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use http\Client\Response;
+
+class VerifyAuthController extends Controller
+{
+    /**
+     * @return Response
+     */
+    public function __invoke(): Response
+    {
+        $loggedIn = Auth::check();
+
+        if (!$loggedIn) {
+            return response()->json([
+                'message' => 'User is not authenticated',
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'User is authenticated',
+        ], 200);
+    }
+}
