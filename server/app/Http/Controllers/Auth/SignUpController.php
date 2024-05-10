82a275c834975c01387b15dfb8fce98e34d81495
take-cantik <take.cantik17@gmail.com>
2024-05-10 19:42:24 +0900

    :sparkles: Add SignUpController

---
+++ server/app/Http/Controllers/Auth/SignUpController.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use http\Client\Request;
+use http\Client\Response;
+
+class SignUpController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function __invoke(Request $request): Response
+    {
+        $email = $request->email;
+        $password = $request->password;
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        User::query()->insert([
+            'email' => $email,
+            'password' => $hashedPassword,
+        ]);
+
+        $loggedIn = Auth::attempt([
+            'email' => $email,
+            'password' => $password,
+        ]);
+
+        if (!$loggedIn) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'User created successfully',
+        ], 201);
+    }
+}
