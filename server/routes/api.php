c4ff4c4f6067b97f83c44e31dd158c859d7e42e8
take-cantik <take.cantik17@gmail.com>
2024-05-10 20:17:21 +0900

    :sparkles: set path

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignUpController;
+use App\Http\Controllers\VerifyAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +22,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/health-check', fn () => 'ok');
 
+Route::post('/login', LoginController::class);
+Route::post('/signup', SignUpController::class);
+Route::get('/verify-auth', VerifyAuthController::class);
