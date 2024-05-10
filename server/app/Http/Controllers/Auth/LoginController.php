4a8addb8cc6647d547e253c5dd2fe0cc952f1d08
take-cantik <take.cantik17@gmail.com>
2024-05-10 19:55:35 +0900

    :sparkles: Add login controller

---
+++ server/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use http\Client\Request;
+use http\Client\Response;
+
+class LoginController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function __invoke(Request $request): Response
+    {
+        $email = $request->email;
+        $password = $request->password;
+
+        $user = User::query()->where('email', $email)->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $passwordMatch = password_verify($password, $user->password);
+
+        if (!$passwordMatch) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'User logged in successfully',
+        ], 201);
+    }
+}
