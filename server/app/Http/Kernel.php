e7eb0941178b28bbbd9c90b91208368930bba982
take-cantik <take.cantik17@gmail.com>
2024-05-14 16:46:42 +0900

    :sparkles: add sanctum

--- server/app/Http/Kernel.php
+++ server/app/Http/Kernel.php
@@ -39,11 +39,9 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
         ],
     ];
 
